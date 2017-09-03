d80e0375914dcd957a6b3e4b8e490a86ee8f5505
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-03 11:16:26 -0400

    chore: scaffold library

---
+++ .scripts/docs.js
@@ -0,0 +1,10 @@
+const { join } = require('path')
+const { readdirSync } = require('fs')
+
+const { generateDocs } = require('./docs/generateDocs')
+
+const ROOT_DIRECTORY = join(__dirname, '..')
+
+generateDocs(ROOT_DIRECTORY)
+
+console.log('Done!')
---
+++ README.md
@@ -0,0 +1,14 @@
+# @typed/history -- 0.0.0
+
+Functional History API for the browser and node
+
+## Get it
+```sh
+yarn add @typed/history
+# or
+npm install --save @typed/history
+```
+
+## API Documentation
+
+All functions are curried!
